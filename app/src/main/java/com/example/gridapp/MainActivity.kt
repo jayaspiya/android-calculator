125a3869636264353581b3aa75759f083379772e
Jayas Piya <pzayazz@gmail.com>
2021-06-15 10:54:17 +0545

    Binding and Basic Listener

--- app/src/main/java/com/example/gridapp/MainActivity.kt
+++ app/src/main/java/com/example/gridapp/MainActivity.kt
@@ -25,6 +25,37 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        btn1 = findViewById(R.id.btn1)
+        btn2 = findViewById(R.id.btn2)
+        btn3 = findViewById(R.id.btn3)
+        btn4 = findViewById(R.id.btn4)
+        btn5 = findViewById(R.id.btn5)
+        btn6 = findViewById(R.id.btn6)
+        btn7 = findViewById(R.id.btn7)
+        btn8 = findViewById(R.id.btn8)
+        btn9 = findViewById(R.id.btn9)
+        btn0 = findViewById(R.id.btn0)
+        btnAdd = findViewById(R.id.btnAdd)
+        btnSub = findViewById(R.id.btnSub)
+        btnProduct = findViewById(R.id.btnProduct)
+        btnDot = findViewById(R.id.btnDot)
+        btnPercent = findViewById(R.id.btnPercent)
+        btnProcess = findViewById(R.id.btnProcess)
+        btn1.setOnClickListener{}
+        btn2.setOnClickListener{}
+        btn3.setOnClickListener{}
+        btn4.setOnClickListener{}
+        btn5.setOnClickListener{}
+        btn6.setOnClickListener{}
+        btn7.setOnClickListener{}
+        btn8.setOnClickListener{}
+        btn9.setOnClickListener{}
+        btn0.setOnClickListener{}
+        btnAdd.setOnClickListener{}
+        btnSub.setOnClickListener{}
+        btnProduct.setOnClickListener{}
+        btnDot.setOnClickListener{}
+        btnPercent.setOnClickListener{}
+        btnProcess.setOnClickListener{}
     }
 }
