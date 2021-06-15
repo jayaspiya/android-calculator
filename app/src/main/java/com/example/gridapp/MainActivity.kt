c836e728b9d901330a1576ddcd81a28461b194d2
Jayas Piya <pzayazz@gmail.com>
2021-06-15 10:53:44 +0545

    Grid Layout Calculator Button

---
+++ app/src/main/java/com/example/gridapp/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.gridapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var btn1: Button
+    private lateinit var btn2: Button
+    private lateinit var btn3: Button
+    private lateinit var btn4: Button
+    private lateinit var btn5: Button
+    private lateinit var btn6: Button
+    private lateinit var btn7: Button
+    private lateinit var btn8: Button
+    private lateinit var btn9: Button
+    private lateinit var btn0: Button
+    private lateinit var btnAdd: Button
+    private lateinit var btnSub: Button
+    private lateinit var btnProduct: Button
+    private lateinit var btnDot: Button
+    private lateinit var btnPercent: Button
+    private lateinit var btnProcess: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
