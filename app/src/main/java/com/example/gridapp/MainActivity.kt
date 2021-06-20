26d20441cf8c02e276985e5f57ff03e6579eb860
Jayas Piya <pzayazz@gmail.com>
2021-06-20 13:58:06 +0545

    Added Dot Funcionality

--- app/src/main/java/com/example/gridapp/MainActivity.kt
+++ app/src/main/java/com/example/gridapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.gridapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 
@@ -24,12 +25,14 @@ class MainActivity : AppCompatActivity() {
     private lateinit var btnPercent: Button
     private lateinit var btnProcess: Button
     private lateinit var btnDecimal: Button
+
     // Calculation Logic
-    private var firstNum: Double = 0.0
-    private var secondNum: Double = 0.0
+    private var firstNum: String = "0"
+    private var secondNum: String = "0"
     private var processToDo: String = ""
     private var answer: Double = 0.0
     private var isReset: Boolean = true
+    private var isDecimal: Boolean = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -52,133 +55,159 @@ class MainActivity : AppCompatActivity() {
         btnProcess = findViewById(R.id.btnProcess)
         btnDecimal = findViewById(R.id.btnDecimal)
         btn1.setOnClickListener {
-            setNumber(1)
+            setNumber("1")
             setOutput()
             isReset = true
         }
         btn2.setOnClickListener {
-            setNumber(2)
+            setNumber("2")
             setOutput()
             isReset = true
         }
         btn3.setOnClickListener {
-            setNumber(3)
+            setNumber("3")
             setOutput()
             isReset = true
         }
         btn4.setOnClickListener {
-            setNumber(4)
+            setNumber("4")
             setOutput()
             isReset = true
         }
         btn5.setOnClickListener {
-            setNumber(5)
+            setNumber("5")
             setOutput()
             isReset = true
         }
         btn6.setOnClickListener {
-            setNumber(6)
+            setNumber("6")
             setOutput()
             isReset = true
         }
         btn7.setOnClickListener {
-            setNumber(7)
+            setNumber("7")
             setOutput()
             isReset = true
         }
         btn8.setOnClickListener {
-            setNumber(8)
+            setNumber("8")
             setOutput()
             isReset = true
         }
         btn9.setOnClickListener {
-            setNumber(9)
+            setNumber("9")
             setOutput()
             isReset = true
         }
         btn0.setOnClickListener {
-            setNumber(0)
+            setNumber("0")
             setOutput()
             isReset = true
         }
+        btnDecimal.setOnClickListener {
+            setNumber(".")
+            setOutput()
+            isDecimal = true
+            btnDecimal.isEnabled = false
+        }
         btnAdd.setOnClickListener {
             processToDo = "+"
-            if (!isReset){
-                firstNum = answer
+            if (!isReset) {
+                firstNum = answer.toString()
                 isReset = true
             }
             toggleProcessBtn()
+            isDecimal = false
+            btnDecimal.isEnabled = true
         }
         btnSub.setOnClickListener {
             processToDo = "-"
-            if (!isReset){
-                firstNum = answer
+            if (!isReset) {
+                firstNum = answer.toString()
                 isReset = true
             }
             toggleProcessBtn()
-
+            isDecimal = false
+            btnDecimal.isEnabled = true
         }
         btnProduct.setOnClickListener {
             processToDo = "*"
-            if (!isReset){
-                firstNum = answer
+            if (!isReset) {
+                firstNum = answer.toString()
                 isReset = true
             }
             toggleProcessBtn()
-
+            isDecimal = false
+            btnDecimal.isEnabled = true
         }
         btnClear.setOnClickListener {
             setResult("")
             resetCalc()
             isReset = true
+            isDecimal = false
+            btnDecimal.isEnabled = true
         }
         btnPercent.setOnClickListener {
             processToDo = "%"
-            if (!isReset){
-                firstNum = answer
+            if (!isReset) {
+                firstNum = answer.toString()
                 isReset = true
             }
             toggleProcessBtn()
-
+            isDecimal = false
+            btnDecimal.isEnabled = true
         }
         btnProcess.setOnClickListener {
-            when(processToDo){
-                "+" -> answer = firstNum + secondNum
-                "-" -> answer = firstNum - secondNum
-                "*" -> answer = firstNum * secondNum
-                "%" -> answer = firstNum * 100 / secondNum
+            when (processToDo) {
+                "+" -> answer = firstNum.toDouble() + secondNum.toDouble()
+                "-" -> answer = firstNum.toDouble() - secondNum.toDouble()
+                "*" -> answer = firstNum.toDouble() * secondNum.toDouble()
+                "%" -> answer = firstNum.toDouble() * 100 / secondNum.toDouble()
             }
             setResult("$answer")
             isReset = false
+            isDecimal = false
+            btnDecimal.isEnabled = true
             resetCalc()
         }
     }
-    private fun setOutput(){
-        if(processToDo == ""){
-            setResult(" ${String.format("%.0f", firstNum)}")
-        }
-        else{
-            setResult("${String.format("%.0f", secondNum)}")
+
+    private fun setOutput() {
+        if (processToDo == "") {
+            if (isDecimal) {
+                setResult(" ${firstNum.toDouble()} ")
+            } else {
+                setResult(" ${String.format("%.0f", firstNum.toDouble())}")
+            }
+        } else {
+            if (isDecimal) {
+                setResult(" ${secondNum.toDouble()} ")
+            } else {
+                setResult("${String.format("%.0f", secondNum.toDouble())}")
+            }
         }
     }
+
     private fun setResult(result: String) {
         tvResult.text = result
     }
-    private fun setNumber(n: Int){
-        if(processToDo == ""){
-            firstNum = (firstNum*10)+n
-        }
-        else{
-            secondNum = (secondNum*10)+n
+
+    private fun setNumber(n: String) {
+        if (processToDo == "") {
+            firstNum += n
+        } else {
+            secondNum += n
         }
     }
-    private fun resetCalc(){
-        firstNum = 0.0
-        secondNum = 0.0
+
+    private fun resetCalc() {
+        firstNum = "0"
+        secondNum = "0"
         processToDo = ""
         toggleProcessBtn(true)
     }
-    private fun toggleProcessBtn(btnState: Boolean = false){
+
+    private fun toggleProcessBtn(btnState: Boolean = false) {
         btnAdd.isEnabled = btnState
         btnProduct.isEnabled = btnState
         btnSub.isEnabled = btnState
